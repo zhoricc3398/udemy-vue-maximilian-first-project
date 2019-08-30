d1b7f87fe3e5b9685c3349148e510f54b61a64e8
zhoricc3398 <16200126@ibsu.edu.ge>
2019-08-30 11:52:42 +0400

    feat(source): finish 54

--- app.js
+++ app.js
@@ -16,12 +16,21 @@ new Vue({
       if (this.checkWin()) {
         return;
       }
+      this.monsterAttacks();
+    },
+    specialAttack() {
+      this.monsterHealth -= this.calculateDamage(10, 20);
+      if (this.checkWin()) {
+        return;
+      }
+      this.monsterAttacks();
+    },
+    heal() {},
+    giveUp() {},
+    monsterAttacks() {
       this.playerHealth -= this.calculateDamage(5, 12);
       this.checkWin();
     },
-    specialAttack() {},
-    heal() {},
-    giveUp() {},
     calculateDamage(min, max) {
       return Math.max(Math.floor(Math.random() * max) + 1, min);
     },
